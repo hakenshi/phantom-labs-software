a3cd033f52fc18a4abb85e19900bbdc97598c738
hakenshi <fkdias025@gmail.com>
2025-08-25 13:40:01 -0300

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-b from-zinc-800 to-zinc-950 min-h-screen text-white`}
       >
         {children}
       </body>
